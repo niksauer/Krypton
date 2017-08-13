c05994cff5e748ce2bb355263f7c99db01ee0ddd
Niklas Sauer <nik.sauer@me.com>
2017-08-13 13:36:59 +0200

    Initial Commit

---
+++ Krypton/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Krypton
+//
+//  Created by Niklas Sauer on 13.08.17.
+//  Copyright © 2017 Hewlett Packard Enterprise. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
